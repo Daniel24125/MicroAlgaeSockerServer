fd5e47fb1bf884f66eaa9da5ab146a2057da25b6
Daniel24125 <danielmadalena@msn.com>
2024-05-20 18:26:53 +0100

    init

---
+++ server.py
@@ -0,0 +1,84 @@
+import socket
+import time 
+import select
+import json
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 3000  # Port to listen on (non-privileged ports are > 1023)
+
+class ServerSocker: 
+    nir_socket = None
+
+    def __init__(self, port=PORT): 
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sockets_list = [self.server_socket]
+        self.server_socket.bind((HOST, port))
+        self.server_socket.listen()
+        print(f"Server listenning on {port}")
+
+    def receive_cmd(self, client_socket):
+        data = client_socket.recv(1024)
+        if not len(data): 
+            raise Exception("An error occured while trying to receive a command, probably due to client disconnection.")
+        cmd = json.loads(data)
+        print(f"Command received: {cmd}")
+        return cmd
+    
+    def parse_cmd(self, cmd, client_socket):
+        if not "cmd" in cmd: 
+            raise Exception("Invalid JSON received")
+        cmd_received = cmd["cmd"]
+        if  cmd_received == "identification": 
+            if cmd["data"] == "nir": 
+                self.nir_socket = client_socket
+
+
+
+    def listen_for_connections(self): 
+        while True: 
+            read_socket, _ , exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
+            for notified_socket in read_socket: 
+                if notified_socket == self.server_socket: 
+                    client_socket, client_address = self.server_socket.accept()
+                    print(f"A client has connected with the follwoing address: {client_address}")
+                    # client_socket.send(b"Welcome to the python server socket")
+                    cmd = self.receive_cmd(client_socket)
+                    self.socket_list.append(client_socket)
+                    self.parse_cmd(cmd, client_socket)
+            print("New iteration")
+            time.sleep(1)
+
+if __name__ == "__main__": 
+    try:
+        server = ServerSocker()
+        server.listen_for_connections()
+    except Exception as e: 
+        print(e)
+
+
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket.bind((HOST, PORT))
+    # server_socket.listen()
+    # print(f"Server listenning on {PORT}")
+    # client_socket, addr = server_socket.accept()
+    # print(f"Client connected by {addr}")
+    # client_socket.send(b"Welcome to the python server socket")
+    # data = client_socket.recv(1024)
+    # cmd = json.loads(data)
+
+    # print(cmd["cmd"])
+    # while True:
+    #     read_socket, _ , exception_sockets = select.select()
+        # with client_socket:
+            # print(f"Client connected by {addr}")
+            # client_socket.send(b"Welcome to the python server socket")
+            # try:
+            #     data = client_socket.recv(1024)
+            #     if not data:
+            #         break
+            #     print(data)
+            #     # client_socket.sendall(data)
+            # except Exception as e:
+            #     print("Client disconnected with the following error", repr(e))
+            #     break
