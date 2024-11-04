86af3bf176c409918b5c555bd0262f5be40f41b1
Daniel24125 <danielmadalena@msn.com>
2024-11-04 13:58:15 +0000

    fixing the logs on the main.py file

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ async def handle_websocket(websocket, path):
     print("Connection made!", path)
     try:
         async for message in websocket:
-            print(f"Received WebSocket message: {message}")
+            logger.log(f"Received WebSocket message: {message}", "MAIN - Nextjs Connection", "info")
             await websocket.send(f"Echo: {message}")
     except websockets.exceptions.ConnectionClosed:
-        print("WebSocket connection closed")
+        logger.log("A client was disconnected.", "MAIN - Nextjs Connection", "info")
 
 
 
 async def handle_tcp(reader, writer):
     print("Connection made!")
+    logger.log("A device connection was made", "MAIN - NIR Connection", "info")
 
     while True:
         data = await reader.read(100)
         if not data:
             break
         message = data.decode()
-        print(f"Received TCP message: {message}")
+        logger.log(f"Received TCP message: {message}", "MAIN - NIR Connection", "info")
+
         writer.write(f"Echo: {message}".encode())
         await writer.drain()
     writer.close()
@@ -33,10 +35,9 @@ async def handle_tcp(reader, writer):
 async def main():
 
     websocket_server = await websockets.serve(handle_websocket, HOST, NEXTJS_PORT)
-    print("NEXTJS Server listenning on port " + str(NEXTJS_PORT))
+    logger.log("NEXTJS Server listenning on port " + str(NEXTJS_PORT), "MAIN", "info")
     tcp_server = await asyncio.start_server(handle_tcp, HOST, CPP_PORT)
-    print("NIR Server listenning on port " + str(CPP_PORT))
-    
+    logger.log("NIR Server listenning on port " + str(CPP_PORT), "MAIN", "info")    
     await asyncio.gather(websocket_server.wait_closed(), tcp_server.serve_forever())
 
 
